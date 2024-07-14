563f1952d42dc3c7145d3d3a5cd29767fbde0320
AngVinter <angieabcdefg2013@gmail.com>
2024-07-13 22:05:12 -0500

    Segundo

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,3 @@
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-
-// Polyfills de ES6 para compatibilidad con navegadores antiguos.
 import 'core-js/es/symbol';
 import 'core-js/es/object';
 import 'core-js/es/function';
@@ -16,21 +11,4 @@ import 'core-js/es/array';
 import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/set';
-
-// Importa estos polyfills si necesitas soporte para IE11 y navegadores más antiguos.
-// import 'classlist.js';  // Ejecuta `npm install --save classlist.js`.
-// import 'web-animations-js';  // Ejecuta `npm install --save web-animations-js`.
-
- /***************************************************************************************************
- * Zone JS es requerida por Angular.
- */
-import 'zone.js';  // Incluido con Angular CLI.
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-/**
- * Este archivo puede ser utilizado para incluir otros polyfills necesarios para tu aplicación.
- * Por ejemplo, puedes importar bibliotecas de terceros que necesitan polyfills.
- */
+import 'zone.js'; 
